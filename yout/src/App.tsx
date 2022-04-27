f0756e9fe7d00a88c88dda6f7f961c389e7bf8d5
Ernis <e.badmaev@gmail.com>
2022-04-27 23:38:16 +0300

    fixModuleStyle

--- yout/src/App.tsx
+++ yout/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Sidebar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Sidebar />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- yout/src/components/Sidebar/Sidebar.tsx
+++ yout/src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,50 @@
 import styles from './Sidebar.module.scss';
+import samanthaImg from '../../assets/png/samantha.png';
+import { useLocation, Link } from 'react-router-dom';
+
+const sidebarNavLinks = [
+  'dashboard',
+  'expenses',
+  'wallets',
+  'summary',
+  'accounts',
+  'settings',
+];
 
 export default function Sidebar() {
+  const location = useLocation();
   return (
     <>
       <aside className={styles.sidebar}>
         <div className={styles.sidebarContent}>
           <div className={styles.profileDetails}>
-            <img src='' alt='samantha' />
+            <img src={samanthaImg} alt="samantha" />
+            <p className={styles.notifications}>4</p>
           </div>
+          <p className={styles.userName}>Samantha</p>
+          <p className={styles.userEmail}>Samantha@email.com</p>
         </div>
+        <nav className={styles.sidebarNav}>
+          <ul>
+            {sidebarNavLinks.map((sidebarNavLink) => {
+                console.log(sidebarNavLink.charAt(0).toUpperCase() + sidebarNavLink.slice(1));//непонятно? -sidebarNavLink возвращает итак строковый элемент   
+              return <li key={sidebarNavLink} className={styles.sidebarNavItem}>
+                <Link
+                  className={
+                    location.pathname === `/${sidebarNavLink}`
+                    ? styles.sidebarNavLinkActive
+                    : styles.sidebarNavLink
+                  }
+                  to={`${sidebarNavLink}`}
+                  >
+                  {sidebarNavLink.charAt(0).toUpperCase() + sidebarNavLink.slice(1)} 
+                  
+                </Link>
+                
+              </li>;
+            })}
+          </ul>
+        </nav>
       </aside>
     </>
   );
